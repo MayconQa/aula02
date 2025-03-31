b069458e4e63d1d788aaf2c4391a438c46dbbdb6
Maycon <mayconfabricio2009@gmail.com>
2025-03-31 18:22:36 -0300

    feat: commit da media float

---
+++ mediadoisnumero.py
@@ -0,0 +1,4 @@
+numero_1 = float(input("Digite primeiro número: "))
+numero_2 = float(input("Digite o segundo número: "))
+media = (numero_1 + numero_2) / 2
+print(f"A média do npumero {numero_1} e {numero_2} é: {media}")
