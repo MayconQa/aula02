298cfd5cca7bd748db6e6e46d1dac756f8f7b638
Maycon <mayconfabricio2009@gmail.com>
2025-03-31 18:24:15 -0300

    feat: commit para adcionar adição float

---
+++ adicaofloat.py
@@ -0,0 +1,4 @@
+numero_1 = float(input("Digite o primeiro número: "))
+numero_2 = float(input("Digite o Segundo número: "))
+soma = numero_1 + numero_2
+print(f"A soma do {numero_1} + {numero_2} e: {soma}")
