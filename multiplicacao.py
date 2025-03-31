69e8011b5f31c3f26b96bf38a303cb4d567bbe9a
Maycon <mayconfabricio2009@gmail.com>
2025-03-31 16:55:15 -0300

    feat: Commit da multiplicação

---
+++ multiplicacao.py
@@ -0,0 +1,6 @@
+numero_1 = int(input("Digite primeiro número para ser multiplicado: "))
+numero_2 = int(input("Digite segundo número para ser multiplicado: "))
+
+mutiplicacao = numero_1 * numero_2
+
+print(f"Resultado da multiplicação: {mutiplicacao}")
