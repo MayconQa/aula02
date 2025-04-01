a22bba78e87e0046ba55075e2cba5c1bd5c613c7
Maycon <mayconfabricio2009@gmail.com>
2025-04-01 10:33:26 -0300

    feat: adicionado arquivo de concatenação

---
+++ concatenacao.py
@@ -0,0 +1,6 @@
+frase1 = input("Digite em qual estado que gostaria de viajar: ")
+frase_2 = input("Digite qual meio de transporte gostaria de viajar: ")
+
+juntar_frases = frase1 + " " + frase_2
+
+print(juntar_frases)
