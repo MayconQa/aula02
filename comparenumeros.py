e7893fc5aeb9a1640905ba2211c84e34f7ac139c
Maycon <mayconfabricio2009@gmail.com>
2025-04-01 21:21:21 -0300

    feat: comparar números

---
+++ comparenumeros.py
@@ -0,0 +1,9 @@
+numero1 = float(input("Digite o primeiro número: "))
+numero2  = float(input("Digite o segundo número: "))
+
+# comparar números
+if numero1 == numero2:
+    print("Os numeros são iguais. ")
+else:
+    print("Os números são diferentes")
+
