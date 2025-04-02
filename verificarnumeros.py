3b16b4ed029eb16ab439a7e62a3eaea22abdf424
Maycon <mayconfabricio2009@gmail.com>
2025-04-01 21:26:46 -0300

    feat: verificando sois números

---
+++ verificarnumeros.py
@@ -0,0 +1,9 @@
+# Solicita dois números ao usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Verifica se os números são diferentes
+if num1 != num2:
+    print("Os números são diferentes.")
+else:
+    print("Os números são iguais.")
