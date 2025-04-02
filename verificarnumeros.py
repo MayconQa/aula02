28b674feff5f2474bc229e913e36918aa0a4d2fd
Maycon <mayconfabricio2009@gmail.com>
2025-04-02 13:40:58 -0300

    atualizando o programa adicionado um try except

--- verificarnumeros.py
+++ verificarnumeros.py
@@ -1,9 +1,21 @@
-# Solicita dois números ao usuário
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
 
-# Verifica se os números são diferentes
-if num1 != num2:
-    print("Os números são diferentes.")
+try:
+    # Solicita dois números ao usuário
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    # Verifica se os números são diferentes
+    if num1 != num2:
+        print("Os números são diferentes.")
+    else:
+        print("Os números são iguais.")
+
+except ValueError:
+    print("Erro: Você deve digitar apenas números válidos.")
+
 else:
-    print("Os números são iguais.")
+    print("Leitura dos números realizada com sucesso!")
+
+finally:
+     print("Programa finalizado.")
+
+
